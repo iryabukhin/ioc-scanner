85acaa0e525fe43d7fba0e27bab1e5de5ce3e38d
Ivan Ryabukhin <iryabukhin@oggettoweb.com>
2024-03-26 00:42:29 +0300

    minor code clarity improvements

--- scanner/core/conditions.py
+++ scanner/core/conditions.py
@@ -1,47 +1,54 @@
 import re
 from typing import Union
-from datetime import datetime
+from datetime import datetime as dt
 
-from scanner.models import IndicatorItem, IndicatorItemCondition
+from scanner.models import IndicatorItem, IndicatorItemCondition as Condition
 
+from loguru import logger
 
 class ConditionValidator:
     @staticmethod
     def validate_condition(
-            item: IndicatorItem,
-            value_to_check: Union[str, int, float, datetime]
+        item: IndicatorItem,
+        value_to_check: Union[str, int, float, dt]
     ):
         condition = item.condition
         content_type = item.content.type
         content_value = item.content.content
-        preserve_case = item.preserve_case
-        negate = item.negate
-
-        if not preserve_case and content_type == 'string':
-            value_to_check = value_to_check.lower()
-            content_value = content_value.lower()
 
         result = False
 
-        if condition == IndicatorItemCondition.IS:
+        if not item.preserve_case and content_type == 'string':
+            value_to_check = value_to_check.lower()
+            content_value = content_value.lower()
+
+        if content_type == 'date':
+            for var in ['value_to_check', 'content_value']:
+                try:
+                    locals()[var] = dt.strptime(locals()[var], '%Y-%m-%d') if not isinstance(locals()[var], dt) else locals()[var]
+                except ValueError as e:
+                    logger.error(f'Failed converting {var} to datetime: {str(e)}')
+                    return False
+
+        if condition == Condition.IS:
             result = (value_to_check == content_value)
-        elif condition == IndicatorItemCondition.CONTAINS and content_type == 'string':
+        elif condition == Condition.CONTAINS and content_type == 'string':
             result = (content_value in value_to_check)
-        elif condition == IndicatorItemCondition.MATCHES and content_type == 'string':
+        elif condition == Condition.MATCHES and content_type == 'string':
             result = bool(re.match(content_value, value_to_check))
-        elif condition == IndicatorItemCondition.STARTS_WITH and content_type == 'string':
+        elif condition == Condition.STARTS_WITH and content_type == 'string':
             result = value_to_check.startswith(content_value)
-        elif condition == IndicatorItemCondition.ENDS_WITH and content_type == 'string':
+        elif condition == Condition.ENDS_WITH and content_type == 'string':
             result = value_to_check.endswith(content_value)
-        elif condition == IndicatorItemCondition.GREATER_THAN:
+        elif condition == Condition.GREATER_THAN:
             if content_type == 'date':
-                result = datetime.strptime(value_to_check, '%Y-%m-%d') > datetime.strptime(content_value, '%Y-%m-%d')
+                result = value_to_check > content_value
             elif content_type in ['int', 'duration']:
                 result = int(value_to_check) > int(content_value)
-        elif condition == IndicatorItemCondition.LESS_THAN:
+        elif condition == Condition.LESS_THAN:
             if content_type == 'date':
-                result = datetime.strptime(value_to_check, '%Y-%m-%d') < datetime.strptime(content_value, '%Y-%m-%d')
+                result = value_to_check < content_value
             elif content_type in ['int', 'duration']:
                 result = int(value_to_check) < int(content_value)
 
-        return not result if negate else result
+        return not result if item.negate else result
