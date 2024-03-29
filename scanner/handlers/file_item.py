2f1bfc3784c5050bb87ab5a7cd12b8fce2459bd1
Ivan Ryabukhin <ryabuchin@gmail.com>
2024-03-29 03:16:12 +0300

    [WIP] begin adding extended Windows support to FileItemHandler

--- scanner/handlers/file_item.py
+++ scanner/handlers/file_item.py
@@ -7,23 +7,31 @@ from scanner.core import BaseHandler, ConditionValidator
 from scanner.models import IndicatorItem, IndicatorItemOperator as Operator
 from scanner.utils.hash import calculate_hash
 
-# Predefined paths to skip on Linux platforms 
-SKIP_PATHS_FULL = {
-    '/proc', '/dev',
-    '/sys/kernel/debug',
-    '/sys/kernel/slab',
-    '/sys/devices',
-    '/usr/src/linux'
-}
-MOUNTED_DEVICES = {'/media', '/volumes'}
-SKIP_PATHS_END = {'/initctl'}
+from loguru import logger
+
+
+HASH_CHUNK_SIZE = 4096
 
 
 class FileItemHandler(BaseHandler):
+    # Predefined paths to skip on Linux platforms
+    SKIP_PATHS_FULL = {
+        '/proc', '/dev',
+        '/sys/kernel/debug',
+        '/sys/kernel/slab',
+        '/sys/devices',
+        '/usr/src/linux'
+    }
+    MOUNTED_DEVICES = {'/media', '/volumes'}
+    SKIP_PATHS_END = {'/initctl'}
+
+    MAX_FILE_SIZE = 1024 * 1024 * 1024
 
     def __init__(self):
         self.file_cache = {}
         self.default_root_path = '/'
+        self.scan_all_drives = False
+
 
     @staticmethod
     def get_supported_terms() -> List[str]:
@@ -39,15 +47,20 @@ class FileItemHandler(BaseHandler):
         ]
 
     def _build_file_info(self, full_path: str) -> Dict:
-        return {
+        result = {
             'FileName': os.path.basename(full_path),
             'FilePath': os.path.abspath(full_path),
             'FileExtension': os.path.splitext(full_path)[1],
             'SizeInBytes': os.path.getsize(full_path),
-            'Md5sum': calculate_hash(full_path, hashlib.md5),
-            'Sha1sum': calculate_hash(full_path, hashlib.sha1),
-            'Sha256sum': calculate_hash(full_path, hashlib.sha256),
         }
+
+        hash_methods = [hashlib.md5(), hashlib.sha1(), hashlib.sha256()]
+        with open(full_path, 'rb') as f:
+            for chunk in iter(lambda: f.read(HASH_CHUNK_SIZE), b""):
+                for hash_obj in hash_methods:
+                    hash_obj.update(chunk)
+        result.update({f'{h.name.title()}sum': hash_obj.hexdigest() for h in hash_methods})
+        return result
 
     def _populate_cache(self) -> None:
         self.file_cache = {
@@ -76,9 +89,12 @@ class FileItemHandler(BaseHandler):
 
     def _full_scan_filesystem(self, root: str) -> List[Dict]:
         # TODO: Add extensive logging to this method
+        logger.info(f'Begin scanning root path "{root}"')
         if not os.path.exists(root):
+            logger.info(f'Path {root} does not exist, aborting scan...')
             return []
 
+        # TODO: add processing of user-specific environment variables (i.e. %USERPROFILE%, %APPDATA% etc)
         for rootdir, dirs, files in os.walk(root, topdown=True, followlinks=False):
             # Skip paths that start with ..
             new_dirs = []
@@ -86,8 +102,11 @@ class FileItemHandler(BaseHandler):
                 skip = False
                 # Generate a complete path for comparisons
                 full_path = os.path.join(root, dir).lower() + os.sep
-                for skip in SKIP_PATHS_FULL:
+                for skip in self.SKIP_PATHS_FULL:
                     if full_path.startswith(skip):
+                        logger.info(
+                            f'Skipping path "{full_path}" because it starts with "{skip}"'
+                        )
                         skip = True
 
                 if not skip:
@@ -98,13 +117,18 @@ class FileItemHandler(BaseHandler):
             for filename in files:
                 fpath = os.path.join(root, filename)
 
-                fmode = os.stat(fpath).st_mode
+                stat = os.stat(fpath)
+                fmode = stat.st_mode
                 if (stat.S_ISCHR(fmode)
                         or stat.S_ISBLK(fmode)
                         or stat.S_ISFIFO(fmode)
                         or stat.S_ISLNK(fmode)
                         or stat.S_ISSOCK(fmode)
                 ):
+                    continue
+
+                if stat.st_size > self.MAX_FILE_SIZE:
+                    logger.info(f'Skipping file "{fpath}" because its size exceeds {self.MAX_FILE_SIZE} bytes')
                     continue
 
                 yield self._build_file_info(fpath)
