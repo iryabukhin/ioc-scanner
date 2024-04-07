8173be00c6be30d5520c69a965cf2dfa905ddb3c
Ivan Ryabukhin <ryabuchin@gmail.com>
2024-04-07 22:46:16 +0300

    slightly refactor FileItemHandler

--- scanner/handlers/file_item.py
+++ scanner/handlers/file_item.py
@@ -17,14 +17,6 @@ from scanner.utils import OSType
 
 from loguru import logger
 
-if OSType.is_win():
-    try:
-        import win32file
-        import win32api
-    except Exception as e:
-        logger.error(f'Could not import win32api or win32file: {str(e)}')
-
-
 
 HASH_CHUNK_SIZE = 4096
 
@@ -45,7 +37,6 @@ class FileItemHandler(BaseHandler):
     MOUNTED_DEVICES = {'/media', '/volumes'}
     SKIP_PATHS_END = {'/initctl'}
 
-
     MAX_FILE_SIZE_MB = 32
 
     def __init__(self, config: ConfigObject):
@@ -58,17 +49,17 @@ class FileItemHandler(BaseHandler):
     @staticmethod
     def get_supported_terms() -> List[str]:
         return [
-'FileItem/Md5sum',
-'FileItem/Sha1sum',
-'FileItem/Sha256sum',
-'FileItem/FileName',
-'FileItem/FilePath',
-'FileItem/FullPath'
-'FileItem/FileExtension',
-'FileItem/SizeInBytes',
-'FileItem/Created',
-'FileItem/Modified',
-'FileItem/Accessed',
+            'FileItem/Md5sum',
+            'FileItem/Sha1sum',
+            'FileItem/Sha256sum',
+            'FileItem/FileName',
+            'FileItem/FilePath',
+            'FileItem/FullPath',
+            'FileItem/FileExtension',
+            'FileItem/SizeInBytes',
+            'FileItem/Created',
+            'FileItem/Modified',
+            'FileItem/Accessed',
         ]
 
     def _build_file_info(self, full_path: str) -> Dict:
@@ -83,11 +74,12 @@ class FileItemHandler(BaseHandler):
         }
 
         hash_methods = [hashlib.md5(), hashlib.sha1(), hashlib.sha256()]
+        hashes = {h.name: h for h in hash_methods}
         with open(full_path, 'rb') as f:
             for chunk in iter(lambda: f.read(HASH_CHUNK_SIZE), b""):
-                for hash_obj in hash_methods:
+                for hash_obj in hashes.values():
                     hash_obj.update(chunk)
-        result.update({f'{h.name.title()}sum': hash_obj.hexdigest() for h in hash_methods})
+        result.update({f'{h.name.title()}sum': h.hexdigest() for h in hashes.values()})
         return result
 
     def validate(self, items: List[IndicatorItem], operator: Operator) -> bool:
@@ -95,7 +87,6 @@ class FileItemHandler(BaseHandler):
             (i for i in items if i.context.search == 'FileItem/FullPath'),
             None
         )
-        # if FullPath item is present and operator is AND, we can scan only the file specified in the FullPath item
         if fullpath_item is not None and operator is Operator.AND:
             path = fullpath_item.content.content
             if path not in self.file_cache:
@@ -104,7 +95,6 @@ class FileItemHandler(BaseHandler):
             self._populate_cache()
 
         valid_items = list()
-        # TODO: Implement better validation logic here
         for item in items:
             term = item.context.search
             value = self.file_cache.get(term)
@@ -115,92 +105,66 @@ class FileItemHandler(BaseHandler):
 
     def _populate_cache(self, root: Optional[str] = None) -> None:
         if root is None:
-            if OSType.is_win():
-                if self._scan_all_drives:
-                    for drive_base_path in self._get_all_drives():
-                        self._populate_cache(drive_base_path)
-                else:
-                    root = self.WIN_SYS_DRIVE
-            elif OSType.is_linux():
-                root = self.LINUX_DEFAULT_ROOT_PATH
-
+            root = self._get_root_path()
         self.file_cache = {
             i['FileItem/FilePath'] for i in self._recursive_scan(root)
         }
 
     def _recursive_scan(self, root: Optional[str] = None) -> List[Dict]:
-        if root is None:
-            if OSType.is_win():
-                if self._scan_all_drives:
-                    for drive_base_path in self._get_all_drives():
-                        self._recursive_scan(drive_base_path)
-                else:
-                    root = self.WIN_SYS_DRIVE
-            elif OSType.is_linux():
-                root = self.LINUX_DEFAULT_ROOT_PATH
+        root = root or self._get_root_path()
 
         logger.info(f'Begin scanning root path "{root}" ...')
         if not os.path.exists(root):
             logger.info(f'Path {root} does not exist, aborting scan...')
             return []
 
-        # TODO: add processing of user-specific environment variables (i.e. %USERPROFILE%, %APPDATA% etc)
         for rootdir, dirs, files in os.walk(root, topdown=True, followlinks=False):
-            # Skip paths that start with ..
-            new_dirs = []
-            for dir in dirs:
-                skip = False
-                # Generate a complete path for comparisons
-                full_path = os.path.join(root, dir).lower() + os.sep
-                for skip in self.SKIP_PATHS_FULL:
-                    if full_path.startswith(skip):
-                        logger.info(
-                            f'Skipping path "{full_path}" because it starts with "{skip}"'
-                        )
-                        skip = True
-
-                if not skip:
-                    new_dirs.append(dir)
-
-            dirs[:] = new_dirs
+            dirs[:] = [d for d in dirs if not self._should_skip(os.path.join(rootdir, d))]
 
             for filename in files:
-                fpath = os.path.join(root, filename)
-
-                fstat = os.stat(fpath)
-                fmode = fstat.st_mode
-                if (stat.S_ISCHR(fmode)
-                        or stat.S_ISBLK(fmode)
-                        or stat.S_ISFIFO(fmode)
-                        or stat.S_ISLNK(fmode)
-                        or stat.S_ISSOCK(fmode)
-                ):
-                    continue
-
-                if fstat.st_size > self._max_file_size * 1024 * 1024:
-                    logger.info(f'Skipping file "{fpath}" because its size exceeds {self._max_file_size} MB')
-                    continue
-
-                try:
-                    yield self._build_file_info(fpath)
-                except PermissionError as e:
-                    logger.warning(f'Could not scan file "{fpath}" due to permission error: {str(e)}')
-                except Exception as e:
-                    logger.warning(f'Could not scan file "{fpath}" due to unknown error: {str(e)}')
+                fpath = os.path.join(rootdir, filename)
+                if self._should_process_file(fpath):
+                    try:
+                        yield self._build_file_info(fpath)
+                    except PermissionError as e:
+                        logger.warning(f'Could not scan file "{fpath}" due to permission error: {str(e)}')
+                    except Exception as e:
+                        logger.warning(f'Could not scan file "{fpath}" due to unknown error: {str(e)}')
 
     def _get_all_drives(self) -> List[str]:
         return [p.mountpoint for p in psutil.disk_partitions() if p.fstype == 'NTFS']
 
     def _get_creation_time(self, full_path) -> Optional[datetime]:
-        if OSType.is_win():
-            timestamp = os.path.getctime(full_path)
-        else:
-            try:
-                timestamp = stat.st_birthtime
-            except AttributeError:
-                timestamp = stat.st_mtime
-        return datetime.fromtimestamp(timestamp, tz=timezone.utc)
+        try:
+            if OSType.is_win():
+                timestamp = os.path.getctime(full_path)
+            else:
+                stat_result = os.stat(full_path)
+                timestamp = getattr(stat_result, 'st_birthtime', stat_result.st_mtime)
+            return datetime.fromtimestamp(timestamp, tz=timezone.utc)
+        except Exception as e:
+            logger.error(f'Error getting creation time for {full_path}: {e}')
+            return None
 
+    def _get_root_path(self) -> str:
+        if OSType.is_win():
+            return self.WIN_SYS_DRIVE if not self._scan_all_drives else self._get_all_drives()
+        elif OSType.is_linux():
+            return self.LINUX_DEFAULT_ROOT_PATH
+
+    def _should_skip(self, path: str) -> bool:
+        lower_path = path.lower()
+        return any(lower_path.startswith(skip.lower()) for skip in self.SKIP_PATHS_FULL)
+
+    def _should_process_file(self, fpath: str) -> bool:
+        fstat = os.stat(fpath)
+        fmode = fstat.st_mode
+        if (stat.S_ISCHR(fmode) or stat.S_ISBLK(fmode) or stat.S_ISFIFO(fmode) or stat.S_ISLNK(fmode) or stat.S_ISSOCK(fmode)):
+            return False
+        if fstat.st_size > self._max_file_size * 1024 * 1024:
+            logger.info(f'Skipping file "{fpath}" because its size exceeds {self._max_file_size} MB')
+            return False
+        return True
 
 def init(config: ConfigObject):
     return FileItemHandler(config)
