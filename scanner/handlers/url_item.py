df21c722d593fa0f6b712f9b657a471237e1aff5
Ivan Ryabukhin <ryabuchin@gmail.com>
2024-04-15 00:30:37 +0300

    Finish UrlHistoryItem handler

--- scanner/handlers/url_item.py
+++ scanner/handlers/url_item.py
@@ -1,17 +1,18 @@
-import os
+import itertools
 from typing import List, Tuple, Dict, Optional
 
 from scanner.core import BaseHandler, ConditionValidator
 from scanner.config import ConfigObject
 from scanner.models import IndicatorItem, IndicatorItemOperator as Operator, IndicatorItemOperator
-from scanner.utils import OSType
 
 import browser_history
 from urllib.parse import urlparse
 
 from loguru import logger
 
+
 class UrlItemHandler(BaseHandler):
+
     def __init__(self, config: ConfigObject):
         super().__init__(config)
         self._cache = dict()
@@ -21,20 +22,20 @@ class UrlItemHandler(BaseHandler):
     def get_supported_terms() -> List[str]:
         return [
             'UrlHistoryItem/BrowserName',
-            'UrlHistoryItem/BrowserVersion',
-            'UrlHistoryItem/URL'
-            'UrlHistoryItem/PageTitle'
-            'UrlHistoryItem/HostName'
-            'UrlHistoryItem/Username'
-            'UrlHistoryItem/Hidden'
-            'UrlHistoryItem/VisitCount'
-            'UrlHistoryItem/VisitFrom'
-            'UrlHistoryItem/VisitType'
-            'UrlHistoryItem/LastVisitDate'
-            'UrlHistoryItem/LastVisitDateLocal'
-            'UrlHistoryItem/FirstVisitDateLocal'
-            'UrlHistoryItem/Thumbnail'
-            'UrlHistoryItem/IndexedContent'
+            # 'UrlHistoryItem/BrowserVersion',
+            'UrlHistoryItem/URL',
+            'UrlHistoryItem/PageTitle',
+            'UrlHistoryItem/HostName',
+            # 'UrlHistoryItem/Username'
+            'UrlHistoryItem/Hidden',
+            'UrlHistoryItem/VisitCount',
+            # 'UrlHistoryItem/VisitFrom',  # this requires joining the referer table, which is not supported as of now
+            'UrlHistoryItem/VisitType',
+            'UrlHistoryItem/FirstVisitDate',
+            'UrlHistoryItem/LastVisitDate',
+            'UrlHistoryItem/LastVisitDateLocal',
+            # 'UrlHistoryItem/Thumbnail'
+            # 'UrlHistoryItem/IndexedContent'
         ]
 
     def validate(self, items: List[IndicatorItem], operator: IndicatorItemOperator) -> bool:
@@ -56,27 +57,39 @@ class UrlItemHandler(BaseHandler):
         for browser_class in browser_history.utils.get_browsers():
             try:
                 browser = browser_class()
-                outputs = browser.fetch_history()
-                for entry in outputs.histories:
-                    last_visit, url, title = entry
+                output = browser.fetch_history(sort=True, desc=True)
+                visits_by_url = itertools.groupby(output.histories, key=lambda v: v[1])
+                for url, visits in visits_by_url:
+                    visits = list(visits)
+
+                    first_visit = min(visits, key=lambda v: v[0])[0]
+                    last_visit = max(visits, key=lambda v: v[0])[0]
+                    visits_count = len(visits)
+
+                    hostname = None
+                    urlobj = urlparse(url)
+                    if urlobj.scheme in ['http', 'https', 'ftp']:
+                        hostname = urlobj.hostname.removeprefix('www.')
+                    # We use the title of the page on the first visit, although it may differ for later page visits
+                    page_title = visits[0][2]
                     self._cache[url] = {
                         'BrowserName': browser.name,
                         'URL': url,
-                        'PageTitle': title,
-                        'HostName': urlparse(url).hostname,
-                        'Username': entry[2],
-                        'Hidden': entry[3],
+                        'PageTitle': page_title,
+                        'HostName': hostname,
+                        'Hidden': False,
+                        'VisitCount': visits_count,
+                        'VisitType': 0,
                         'LastVisitDate': last_visit,
-                        'LastVisitDateLocal': last_visit.astimezone(tz=None),
+                        'LastVisitDateLocal': last_visit,
+                        'FirstVisitDateLocal': first_visit,
                     }
             except AssertionError as e:
                 logger.debug(f"Browser is not installed, details: {str(e)}")
                 continue
-            except PermissionError as e:
-                name = browser.name if browser is not None else browser_class.__class__.__name__
-                logger.debug(f'Could not fetch history for {name} browser: {str(e)}')
-
+            except (PermissionError, FileNotFoundError) as e:
+                logger.debug(f'Could not fetch history for {browser_class.name} browser: {str(e)}')
 
 
 def init(config: ConfigObject):
-    pass
+    return UrlItemHandler(config)
