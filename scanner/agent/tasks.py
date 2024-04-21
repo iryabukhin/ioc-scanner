dae6f397149a2dc332944c387f7acb223889184b
Ivan Ryabukhin <ryabuchin@gmail.com>
2024-04-22 02:05:05 +0300

    [WIP] update background task runner

--- scanner/agent/tasks.py
+++ scanner/agent/tasks.py
@@ -1,33 +1,46 @@
 import time
 import os
+import pickle
 
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
 from concurrent.futures import ProcessPoolExecutor, as_completed
-from . import create_app
 from .models import db, Task
 from scanner.core import IOCScanner
 
-def process_task(task_id):
-    app = create_app()
-    with app.app_context():
-        task = Task.query.get(task_id)
-        if task:
-            try:
-                result = IOCScanner.process()  # Your processing logic here
-                task.status = "completed"
-            except Exception as e:
-                task.status = "failed"
-                # Log the error
-            finally:
-                db.session.commit()
+from loguru import logger
 
-def task_runner():
+def process_task(session, config, task_id):
+    task = session.query(Task).get(task_id)
+    if task:
+        try:
+            indicators = pickle.loads(task.data_serialized)
+        except pickle.PickleError as e:
+            logger.error(f'Failed to deserialize task: {str(e)}')
+            return
+
+        try:
+            scanner = IOCScanner(config)
+            result = scanner.process(indicators)
+            task.status = 'completed'
+        except Exception as e:
+            task.status = 'failed'
+            logger.error(f'Failed to process task: {str(e)}')
+        finally:
+            session.commit()
+
+def task_runner(db_session, config):
+    logger.info('Starting task runner')
     while True:
-        app = create_app()
-        with app.app_context():
-            tasks = Task.query.filter_by(status='pending').all()
+        try:
+            tasks = db_session.query(Task).filter_by(status='pending').all()
+            logger.info('Fetching and running tasks..')
             with ProcessPoolExecutor() as executor:
-                futures = {executor.submit(process_task, task.id): task for task in tasks}
+                futures = {executor.submit(process_task, db_session, config, task.id): task for task in tasks}
                 for future in as_completed(futures):
-                    # log task completion or handle exceptions, if any occurred
+                    result = future.result()
                     pass
-        time.sleep(10)
+        except Exception as e:
+            logger.exception(f'An error occurred while processing a task: {str(e)}')
+        finally:
+            time.sleep(10)
