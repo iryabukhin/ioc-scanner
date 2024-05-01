a8cde961e31ef3a005c6596e5b2999d888616eba
Ivan Ryabukhin <ryabuchin@gmail.com>
2024-05-02 01:51:26 +0300

    add new routes for YARA rule scanning

--- scanner/agent/views.py
+++ scanner/agent/views.py
@@ -4,6 +4,7 @@ import io
 
 import yara
 from flask import Blueprint, jsonify, request
+from sqlalchemy import select, and_,  or_
 
 from .models import Task, YaraRule
 from .db import db
@@ -13,16 +14,31 @@ from scanner.yara import YaraScanner, SourceType
 views_blueprint = Blueprint('views', __name__)
 
 
-def success(message: str, status: int = 200, data: dict = {}):
+def success(message: str, status: int = 200, data=None):
+    if data is None:
+        data = {}
     return jsonify({'status': 'success', 'message': message, 'data': data}), status
 
 
-def error(message: str, status: int = 400, data: dict = {}):
+def error(message: str, status: int = 400, data=None):
+    if data is None:
+        data = {}
     return jsonify({'status': 'error', 'message': message, 'data': data}), status
+
+def fetch_rule(name_or_id: str):
+    return db.one_or_404(
+        db.select(YaraRule).where(or_(
+            YaraRule.id == name_or_id,
+            YaraRule.name == name_or_id
+        ))
+    )
 
 
 @views_blueprint.route('/tasks/iocscan', methods=['POST'])
 def create_task():
+    if request.content_type not in ['application/xml', 'application/xhtml+xml']:
+        return error('Invalid request content type, must be "application/xml" or "application/xhtml+xml"')
+
     # Assuming the XML data is sent in the request body
     xml_data = request.data
     if not xml_data:
@@ -53,6 +69,13 @@ def get_task_status(task_id: int):
     return jsonify(task.to_dict())
 
 
+@views_blueprint.route('/yara/rules', methods=['GET'])
+def yara_list_all():
+    all_rules = db.session.query().with_entities(YaraRule.id, YaraRule.name, YaraRule.text)
+    rules = {rule[0]: rule[1] for rule in all_rules}
+    return success('fetched all rules', data={'rules': rules})
+
+
 @views_blueprint.route('/yara/add_rule', methods=['POST'])
 def yara_save():
     if not request.is_json:
@@ -60,17 +83,18 @@ def yara_save():
 
     rule_name = request.json.get('name')
     rule_text = request.json.get('text')
-    vars = request.json.get('vars')
+    variables = request.json.get('vars')
 
-    buff = io.BytesIO()
+    buffer = io.BytesIO()
     try:
         scanner = YaraScanner()
-        scanner.compile_rules(rule_text, SourceType.STRING)
-        compiled_rule = scanner.rules.save(file=buff)
+        scanner.compile_rules(rule_text, SourceType.STRING, variables)
+        scanner.rules.save(file=buffer)
+        buffer.seek(0)
         rule = YaraRule(
-            name=rule_name or uuid.uuid4(),
+            name=rule_name or str(uuid.uuid4()),
             text=rule_text,
-            compiled_data=buff.read()
+            compiled_data=buffer.read()
         )
     except yara.Error as e:
         return error(
@@ -83,32 +107,55 @@ def yara_save():
     return success('Rule saved', 201, {'rule_id': rule.id})
 
 
-@views_blueprint.route('yara/<int:rule_id>/pid', methods=['POST'])
-def yara_scan(rule_id: int):
+@views_blueprint.route('/yara/<rule_id_or_name>/pid', methods=['POST'])
+def yara_scan(rule_id_or_name: str):
     if not request.is_json:
         return error('Invalid request content type, must be "application/json"')
 
     variables = request.json.get('variables', {})
     pid = request.json.get('pid')
+    if not pid:
+        return error('No pid provided!')
 
-    scanner = YaraScanner(rule, SourceType.STRING)
-    matches = scanner.scan_process(pid, variables)
-    return success('Finished scan', 200, {'matches': matches})
+    rule = fetch_rule(rule_id_or_name)
+    try:
+        scanner = YaraScanner()
+        scanner.load_compiled_rules(rule.compiled_data)
+    except yara.Error as e:
+        return error(f'Failed to load pre-compiled Yara rule "{rule_id_or_name}"')
+
+    try:
+        matches = scanner.scan_process(pid, variables)
+    except PermissionError as e:
+        return error('Unable to perform a scan due to a permission error', data={'errmsg': str(e)})
+    except Exception as e:
+        return error('Unable to perform a scan due to an unknown error', data={'errmsg': str(e)})
+
+    return success('Scan finished', 200, {'matches': matches})
 
 
-@views_blueprint.route('/yara/<int:rule_id>/file', methods=['GET'])
-def yara_file():
+@views_blueprint.route('/yara/<rule_id_or_name>/file', methods=['POST'])
+def yara_file(rule_id_or_name: str):
     if not request.is_json:
         return error('Invalid request content type, must be "application/json"')
 
-    rule_id = request.json.get('rule_id')
-    rule = request.json.get('rule')
     variables = request.json.get('variables', {})
-    pid = request.json.get('pid')
+    filepath = request.json.get('filepath')
+    if not filepath:
+        return error('No filepath provided!')
 
-    if not all([pid, rule_id, rule]):
-        return error('Missing required parameters!')
+    rule = fetch_rule(rule_id_or_name)
+    try:
+        scanner = YaraScanner()
+        scanner.load_compiled_rules(rule.compiled_data)
+    except yara.Error as e:
+        return error(f'Failed to load pre-compiled Yara rule "{rule_id_or_name}"', data={'errmsg': str(e)})
 
-    scanner = YaraScanner(rule, SourceType.STRING)
-    matches = scanner.scan_process(pid, variables)
-    return success('Finished scan', 200, {'matches': matches})
+    try:
+        matches = scanner.scan_file(filepath, variables)
+    except PermissionError as e:
+        return error('Unable to performa a scan due to a permission error', data={'errmsg': str(e)})
+    except Exception as e:
+        return error('Unable to performa a scan due to an unknown error', data={'errmsg': str(e)})
+
+    return success('Scan finished', 200, {'matches': matches})
--- scanner/yara/scanner.py
+++ scanner/yara/scanner.py
@@ -1,3 +1,5 @@
+import io
+import json
 from typing import Optional
 
 import yara
@@ -26,53 +28,61 @@ class YaraScanner:
 
     @property
     def rules(self) -> yara.Rules:
+        if self._rules is None:
+            raise ValueError(
+                'No rules loaded yet! Compile or load rules first!'
+            )
         return self._rules
 
     def compile_rules(self, rules_source: str, source_type: SourceType, vars: Optional[dict] = None):
         all_rules = None
-        try:
-            if source_type == SourceType.FILE:
-                path = rules_source
-                rule_files = [f for f in os.listdir(path)] if os.path.isdir(path) else path
-                for filename in rule_files:
-                    # nuh-uh, no recursion
-                    if os.path.isdir(filename):
-                        continue
-                    extension = os.path.splitext(filename)[1].lower()
-                    if extension != '.yar' and extension != '.yara':
-                        continue
-                    with open(filename, 'r') as f:
-                        all_rules += yara.compile(source=f.read())
-                return yara.compile(source=all_rules)
-            elif source_type == SourceType.STRING:
-                return yara.compile(source=rules_source)
-            else:
-                raise ValueError("source_type must be either SourceType.FILE or SourceType.STRING")
-        except yara.SyntaxError as e:
-            logger.error(f'Syntax error in YARA rules: {e}')
-        except yara.Error as e:
-            logger.error(f'General YARA error: {e}')
-        except Exception as e:
-            logger.error(f'Unexpected error compiling YARA rules: {e}')
+        vars = {} if vars is None else vars
+        if source_type == SourceType.FILE:
+            path = rules_source
+            rule_files = [f for f in os.listdir(path)] if os.path.isdir(path) else path
+            for filename in rule_files:
+                if os.path.isdir(filename):
+                    continue
+                extension = os.path.splitext(filename)[1].lower()
+                if extension != '.yar' and extension != '.yara':
+                    continue
+                with open(filename, 'r') as f:
+                    all_rules += yara.compile(source=f.read(), externals=vars)
+            self._rules = yara.compile(source=all_rules, externals=vars)
+        elif source_type == SourceType.STRING:
+            self._rules = yara.compile(source=rules_source, externals=vars)
+        else:
+            raise ValueError("source_type must be either SourceType.FILE or SourceType.STRING")
+        return self._rules
+
+
+    def load_compiled_rules(self, data: bytes) -> None:
+        buffer = io.BytesIO()
+        buffer.write(data)
+        buffer.seek(0)
+        self._rules = yara.load(file=buffer)
+
 
     def scan_file(self, file_path: str, rule_variables: Optional[dict[str]] = None):
         if not os.path.exists(file_path):
             raise ValueError(f'File does not exist: {file_path}')
 
+        full_path = os.path.abspath(file_path)
         return self._do_scan({
-            'filepath': file_path,
+            'filepath': full_path,
             'externals': rule_variables
         })
 
     def scan_process(self, pid: int, vars: Optional[dict] = None):
         if vars is None:
             vars = {}
+
         try:
-            proc_data = psutil.Process(pid=pid).memory_info()
-        except Exception as e:
-            raise PermissionError(
-                f'Cannot scan process {pid} due to a permission error'
-            )
+            proc_data = psutil.Process(pid=pid).memory_maps()
+        except psutil.AccessDenied as e:
+            raise PermissionError(f'Cannot scan process {pid} due to a permission error')
+        except psutil.NoSuchProcess as e:
+            raise ValueError(f'No such process with {pid=}')
 
         return self._do_scan({'pid': pid, 'externals': vars})
 
@@ -83,15 +93,15 @@ class YaraScanner:
         )
         return [self._process_match(m) for m in matches]
 
-    def _process_match(self, match: yara.Match) -> tuple:
-        matched_strings = []
+    def _process_match(self, match: yara.Match) -> dict:
+        result = {}
+        matched_strings = {}
         if hasattr(match, 'strings'):
             for string in match.strings:
                 string_value = str(string.instances[0]).replace("'", '\\')
                 if len(string_value) > self.MAX_MATCH_DISPLAY_LEN:
                     string_value = string_value[:self.MAX_MATCH_DISPLAY_LEN] + " ... (truncated)"
-                matched_strings.append(
-                    "{0}: '{1}'".format(string.identifier, string_value)
-                )
-        return match.rule, matched_strings
+                matched_strings[string.identifier] = string_value
+        result[match.rule] = matched_strings
+        return result
 
